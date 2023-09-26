06b3e1bb25be65dbc20fea72669b2ef2244bf4da
Marco <mballalbarran@gmail.com>
2023-09-26 16:05:29 +0000

    #21 changed structure of root module and added tagging to bucket

--- main.tf
+++ main.tf
@@ -1,52 +1,18 @@
-#terraform {
-  #backend "remote" {
-   # hostname = "app.terraform.io"
-    #organization = "Marcoverse"
-
-   # workspaces {
-    #  name = "Terrahouse"
-   # }
-  #}
-terraform {
-  cloud {
-    organization = "Marcoverse"
-
-    workspaces {
-      name = "Terrahouse"
-    }
-  }
-
-
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.16.2"
-    }
-  }
-}
-
-provider "random" {
-  # Configuration options
-}
-provider "aws" {
-  # Configuration options
-}
-
+# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
-  length           = 27
-  special          = false
+  lower = true
   upper = false
+  length   = 27
+  special  = false
 }
-#[S3 Resource Provider](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket)
-resource "aws_s3_bucket" "example" {
-bucket = random_string.bucket_name.result
-  }
-#[S3 Naming Conventions](https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html)
 
-output "random_bucket_name" {
-    value = random_string.bucket_name.result
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
+resource "aws_s3_bucket" "example" {
+  # Bucket Naming Rules
+  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
+  bucket = random_string.bucket_name.result
+
+  tags = {
+    UserUuid = var.user_uuid
+  }
 }
