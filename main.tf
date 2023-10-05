4ed76e59af946b3cb90532687781e416dccedd70
Marco <mballalbarran@gmail.com>
2023-10-05 21:40:59 +0000

    #30 enabled static website hosting. Remade tfvars. uploaded index/error.html via terraform

--- main.tf
+++ main.tf
@@ -17,9 +17,10 @@ terraform {
 
 }
 
-module "terrahouse_aws"{
-    source = "./modules/terrahouse_aws"
-    user_uuid = var.user_uuid
-    bucket_name = var.bucket_name
-
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.user_uuid
+  bucket_name = var.bucket_name
+  index_html_filepath = var.index_html_filepath
+  error_html_filepath = var.error_html_filepath
 }
--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "bucket_name" {
   description = "Bucket name for my static website"
   value = module.terrahouse_aws.bucket_name
 }
+
+output "s3_website_endpoint" {
+  description = "S3 Static Website hosting endpoint"
+  value = module.terrahouse_aws.website_endpoint
+}
