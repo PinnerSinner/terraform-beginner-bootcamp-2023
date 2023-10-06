c0290d3f179045fadd7304cdb4ead6f8aa8d5b4f
Marco <108472081+PinnerSinner@users.noreply.github.com>
2023-10-06 19:02:28 +0100

    #38 upload assets using a for_each (#39)

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -48,6 +48,18 @@ resource "aws_s3_object" "error_html" {
   #}
 }
 
+resource "aws_s3_object" "upload_assets" {
+  for_each = fileset(var.assets_path,"*.{jpg,png,gif}")
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key    = "assets/${each.key}"
+  source = "${var.assets_path}/${each.key}"
+  etag = filemd5("${var.assets_path}${each.key}")
+  lifecycle {
+    replace_triggered_by = [terraform_data.content_version.output]
+    ignore_changes = [etag]
+  }
+}
+
 resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = aws_s3_bucket.website_bucket.bucket
   #policy = data.aws_iam_policy_document.allow_access_from_another_account.json
@@ -75,3 +87,6 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
 resource "terraform_data" "content_version" {
   input = var.content_version
 }
+
+
+#fileset("${path.root}/public/assets","*.{jpg,png,JPG,PNG,gif,jpeg}")
