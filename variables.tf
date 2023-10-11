816b414dbdf71c0c4fa3aa33460363201e89ba09
Marco <mballalbarran@gmail.com>
2023-10-11 22:32:08 +0000

    #49 testing my terrahome on missingo/testing ground :O

--- variables.tf
+++ variables.tf
@@ -1,8 +1,16 @@
-variable "user_uuid" {
-  type        = string
-  }
+variable "terratowns_endpoint" {
+ type = string
+}
 
-  variable "bucket_name" {
+variable "terratowns_access_token" {
+ type = string
+}
+
+variable "teacherseat_user_uuid" {
+ type = string
+}
+
+variable "bucket_name" {
     type        = string
     }
 
