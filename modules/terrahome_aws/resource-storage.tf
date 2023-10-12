77b6ffbaf8cfc3316ccb659f4b153de9858d16e4
Marco <mballalbarran@gmail.com>
2023-10-12 20:39:31 +0000

    #53 headache with munch

--- modules/terrahome_aws/resource-storage.tf
+++ modules/terrahome_aws/resource-storage.tf
@@ -38,7 +38,7 @@ resource "aws_s3_object" "index_html" {
 }
 
 resource "aws_s3_object" "upload_assets" {
-  for_each = fileset("${var.public_path}/assets","*.{jpg,png,gif}")
+  for_each = fileset("${var.public_path}/assets","*.{jpg,png,gif,jpeg}")
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "assets/${each.key}"
   source = "${var.public_path}/assets/${each.key}"
